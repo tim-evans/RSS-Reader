d1e207fe04fb6860c2553643705b33a7818d7319
Tim Evans <tim-evans@sproutcore.com>
2012-01-30 12:51:55 -0500

    reading_list/controllers/feed: when adding a new feed, check if the record was destroyed

--- apps/reading_list/controllers/feed.js
+++ apps/reading_list/controllers/feed.js
@@ -4,7 +4,11 @@ ReadingList.feedController = SC.ArrayController.create({
   addFeed: function (target) {
     ReadingList.AddFeedPane.create({
       action: function (target) {
-        ReadingList.store.find(RSS.Feed, target.get('value'));
+        var record = ReadingList.store.find(RSS.Feed, target.get('value'));
+        if (record.get('isDestroyed')) {
+          ReadingList.store.writeStatus(record.get('storeKey'), SC.Record.READY_CLEAN);
+          record.refresh();
+        }
       }
     }).popup(target, SC.PICKER_MENU_POINTER);
   }
