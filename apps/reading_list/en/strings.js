87baf07027d2d5136e1f94fa789c21164d097dbb
Tim Evans <tim-evans@sproutcore.com>
2012-01-30 12:50:04 -0500

    add localization strings for english

---
+++ apps/reading_list/en/strings.js
@@ -0,0 +1,14 @@
+SC.stringsFor('en', {
+
+  // Add new feed
+  '_Add feed': 'Add feed',
+  '_Go Back': 'Go Back',
+
+  // Feed Manager
+  '_Check All': 'Check All',
+  '_Remove feeds from your list': 'Remove feeds from your list',
+  '_Remove (%@)': 'Remove (%@)',
+  '_Lots': 'Lots',
+  '_Cancel': 'Cancel'
+
+});
