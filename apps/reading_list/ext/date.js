da008ffd492a4d984341c7a3ee2ac2e1eba3da17
Tim Evans <tim-evans@sproutcore.com>
2012-02-06 18:26:36 -0500

    added day-of-week calculations

---
+++ apps/reading_list/ext/date.js
@@ -0,0 +1,26 @@
+/**
+  Returns whether the two days are on the same day.
+  @param {Date}
+  @param {Date}
+ */
+SC.isSameDay = function (now, day) {
+  var today = new Date(now.getFullYear(), now.getMonth(), now.getDate()),
+      time = new Date(day.getFullYear(), day.getMonth(), day.getDate()).getTime();
+  return today.getTime() === time;
+};
+
+SC.isDayBefore = function (now, day) {
+  var today = new Date(now.getFullYear(), now.getMonth(), now.getDate()),
+      time = new Date(day.getFullYear(), day.getMonth(), day.getDate()).getTime();
+  return (today.getTime() - 86400000) === time;
+};
+
+SC.isSameWeek = function (now, day, startsOnMonday) {
+  var today = new Date(now.getFullYear(), now.getMonth(), now.getDate()),
+      time = new Date(day.getFullYear(), day.getMonth(), day.getDate()).getTime(),
+      beginningOfWeek = today.getTime() - today.getDay() * 86400000,
+      endOfWeek = beginningOfWeek + 604800000;
+
+  return time >= beginningOfWeek &&
+         time <= endOfWeek;
+};
