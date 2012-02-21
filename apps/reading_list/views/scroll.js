82faf198c6077cc2e0f694a750db2ab2d0b1f24e
Tim Evans <tim-evans@sproutcore.com>
2012-02-20 19:44:48 -0500

    show track while dragging the thumb; delay hiding the track for 1 second

--- apps/reading_list/views/scroll.js
+++ apps/reading_list/views/scroll.js
@@ -1,3 +1,18 @@
+SC.ScrollerView.reopen({
+  render: function (original, context, firstTime) {
+    context.setClass('active', this._thumbDragging);
+    return original(context, firstTime);
+  }.enhance(),
+
+  mouseUp: function (original, evt) {
+    var wasDraggingThumb = this._thumbDragging,
+        ret = original(evt);
+    if (wasDraggingThumb && !this._thumbDragging) {
+      this.displayDidChange();
+    }
+  }.enhance()
+});
+
 SC.mixin(SC.ScrollerView.prototype, {
   scrollbarThickness: 12,
   buttonLength: 6,
