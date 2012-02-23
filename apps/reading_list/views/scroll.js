f6430019f9b44eb38858ac6e039f8fbe6adcc74e
Tim Evans <tim-evans@sproutcore.com>
2012-02-22 23:26:17 -0500

    added logic to make scrollers hide and show like scrollers on OSX Lion
    note: this needs some work to be readable.
          right now, this works and looks good, so this should be the base for how scrollers should look and feel when refactoring

--- apps/reading_list/views/scroll.js
+++ apps/reading_list/views/scroll.js
@@ -1,13 +1,64 @@
 SC.ScrollerView.reopen({
+
   render: function (original, context, firstTime) {
-    context.setClass('active', this._thumbDragging);
+    if (!this._suppressActiveChecks) {
+      if (!this._isThumbActive) {
+        this._isTrackActive = this._isHovering;
+        this._isThumbActive = true;
+      }
+
+      if (this._activeTimer) {
+        this._activeTimer.invalidate();
+      }
+
+      this._activeTimer = SC.Timer.schedule({
+        target: this,
+        action: '_thumbDidBecomeInactive',
+        interval: 1000
+      });
+    } else {
+      this._suppressActiveChecks = NO;
+    }
+
+    context.setClass('active-track', this._thumbDragging || this._isTrackActive);
+    context.setClass('active-thumb', this._thumbDragging || this._isThumbActive);
     return original(context, firstTime);
   }.enhance(),
+
+  _thumbDidBecomeInactive: function () {
+    if (!this._isHovering) {
+      this._isTrackActive = false;
+      this._isThumbActive = false;
+      this._activeTimer = null;
+      this._suppressActiveChecks = YES;
+      this.displayDidChange();
+    } else {
+      this._hideOnMouseOut = true;
+    }
+  },
+
+  mouseEntered: function () {
+    this._isHovering = true;
+    if (!this._isTrackActive && this._isThumbActive) {
+      this._isTrackActive = true;
+      this._suppressActiveChecks = YES;
+      this.displayDidChange();
+    }
+  },
+
+  mouseExited: function () {
+    this._isHovering = false;
+    if (this._hideOnMouseOut) {
+      this.displayDidChange();
+      this._hideOnMouseOut = false;
+    }
+  },
 
   mouseUp: function (original, evt) {
     var wasDraggingThumb = this._thumbDragging,
         ret = original(evt);
     if (wasDraggingThumb && !this._thumbDragging) {
+      this._suppressActiveChecks = YES;
       this.displayDidChange();
     }
   }.enhance()
