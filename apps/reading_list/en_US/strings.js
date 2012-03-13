67367ba60d2739de4ef664d4ca6fbbc597218274
Tim Evans <tim-evans@sproutcore.com>
2012-03-13 16:26:16 -0400

    updating strings for the app

--- apps/reading_list/en_US/strings.js
+++ apps/reading_list/en_US/strings.js
@@ -1,16 +1,25 @@
 SC.stringsFor('en_US', {
 
   "_There's nothing to read.": "There's nothing to read.",
+  "_Back": "&larr; Back",
+
+  "_My Feeds": "My Feeds",
 
   // Add new feed
-  '_Add feed': 'Add feed',
-  '_Back': '&larr; Back',
+  "_Add": "Add",
+  "_Go Back": "Go Back",
+  "_Add a new feed": "Add a new feed",
 
   // Feed Manager
-  '_Check All': 'Check All',
-  '_Remove feeds from your list': 'Remove feeds from your list',
-  '_Remove (%@)': 'Remove (%@)',
-  '_Lots': 'Lots',
-  '_Cancel': 'Cancel'
+  "_Check All": "Check All",
+  "_Remove feeds from your list": "Remove feeds from your list",
+  "_Remove (%@)": "Remove (%@)",
+  "_Lots": "Lots",
+  "_Cancel": "Cancel",
+
+  // Settings
+  "_Settings": "Settings",
+  "_Show feed icons": "Show feed icons",
+  "_Done": "Done"
 
 });
